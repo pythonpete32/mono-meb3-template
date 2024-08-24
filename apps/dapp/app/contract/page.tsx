f6c604ac14e25e3db3bad9126923fb0dc625c77d
pythonpete32 <aaron@DAOresear.ch>
2024-08-24 13:45:28 +0400

    feat: :sparkles: add Contract page

---
+++ apps/dapp/app/contract/page.tsx
@@ -0,0 +1,9 @@
+import SimpleStorageComponent from "../../components/SimpleStorage";
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="flex flex-col items-center pt-60 min-h-screen">
+      <SimpleStorageComponent />
+    </main>
+  );
+}
