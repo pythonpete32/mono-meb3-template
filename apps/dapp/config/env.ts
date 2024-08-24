5610c0ab3b7da2c85c0e2c7d27f64ee00678d576
pythonpete32 <aaron@DAOresear.ch>
2024-08-24 12:23:46 +0400

    feat: :zap: add suport for tenderly in the dapp

--- apps/dapp/config/env.ts
+++ apps/dapp/config/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envVariables = z.object({
   NEXT_PUBLIC_ALCHEMY_ID: z.string(),
   NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string(),
+  NEXT_PUBLIC_TENDERLY_RPC: z.string(),
 });
 
 envVariables.parse(process.env);
